18ccee9dd5a7c02503526f63a3d5b681bbc77559
qweliant <dmtorcode@tutanota.com>
2025-01-25 04:39:26 -0500

    Made a picasso?

--- frontend/src/components/blocks/content/properties-sidebar.tsx
+++ frontend/src/components/blocks/content/properties-sidebar.tsx
@@ -1,12 +1,11 @@
-import React, { memo, useState } from "react";
+import { memo, useState } from "react";
 import { useObject } from "@/store/objectsStore";
 import { useQuery } from "@tanstack/react-query";
 import { ObjectType } from "../../../types/objectTypes";
-import { Button } from "../../ui/button";
 import { Label } from "../../ui/label";
 import { Input } from "../../ui/input";
 import { Calendar } from "../../ui/calendar";
-import { CalendarIcon, HashIcon } from "lucide-react";
+import { HashIcon } from "lucide-react";
 import { Switch } from "../../ui/switch";
 import { produce } from "immer";
 import {
@@ -19,18 +18,25 @@ import { useObjectWithSelect } from "@/store/objectsStore";
 
 import ObjectSelect from "../../object-select";
 import { createPortal } from "react-dom";
+
+import { Checkbox } from "@/components/ui/checkbox";
+
 interface PropertiesSidebarProps {
   id: string;
 }
 
 const PropertiesSidebar = memo(
   ({ id }: PropertiesSidebarProps): JSX.Element => {
-    const { data: object, mutate } = useObjectWithSelect(id, 'properties', (object) => {
-      if (!object.properties) {
-        object.properties = {};
+    const { data: object, mutate } = useObjectWithSelect(
+      id,
+      "properties",
+      (object) => {
+        if (!object.properties) {
+          object.properties = {};
+        }
+        return object;
       }
-      return object;
-    });
+    );
     const objectTypeId = object?.type;
     const { data: objectType } = useQuery<ObjectType>({
       queryKey: ["objectType", objectTypeId],
@@ -46,103 +52,64 @@ const PropertiesSidebar = memo(
     return (
       <div className="px-2 py-4 flex flex-col gap-2">
         {object.properties &&
-          Object.entries(object.properties).map(
-            ([key, property]) => {
-              if (objectType.properties[key].type === "text") {
-                return (
-                  <div key={key}>
-                    <Label>{objectType.properties[key].name}</Label>
+          Object.entries(object.properties).map(([key, property]) => {
+            if (objectType.properties[key].type === "text") {
+              return (
+                <div key={key}>
+                  <Label>{objectType.properties[key].name}</Label>
+                  <Input
+                    value={property.value}
+                    onChange={(e) => {
+                      const draft = { ...object };
+                      const newObject = produce(draft, (draft) => {
+                        if (!draft.properties) {
+                          draft.properties = {};
+                        }
+                        draft.properties[key].value = e.target.value;
+                      });
+                      mutate(newObject);
+                    }}
+                  />
+                </div>
+              );
+            } else if (objectType.properties[key].type === "boolean") {
+              return (
+                <div key={key} className="flex flex-col gap-2 py-2">
+                  <Label>{objectType.properties[key].name}</Label>
+                  {/* <Checkbox checked={value === "true"} /> */}
+                  <Switch />
+                </div>
+              );
+            } else if (objectType.properties[key].type === "number") {
+              return (
+                <div key={key}>
+                  <Label>{objectType.properties[key].name}</Label>
+                  <div className="relative">
                     <Input
-                      value={property.value}
+                      value={property.valueNumber}
+                      type="number"
+                      className="pl-10"
                       onChange={(e) => {
                         const draft = { ...object };
                         const newObject = produce(draft, (draft) => {
                           if (!draft.properties) {
                             draft.properties = {};
                           }
-                          draft.properties[key].value = e.target.value;
+                          draft.properties[key].valueNumber = Number(
+                            e.target.value
+                          );
                         });
                         mutate(newObject);
                       }}
                     />
+                    <HashIcon
+                      className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400 "
+                      size={12}
+                    />
                   </div>
-                );
-              } else if (objectType.properties[key].type === "boolean") {
-                return (
-                  <div key={key} className="flex flex-col gap-2 py-2">
-                    <Label>{objectType.properties[key].name}</Label>
-                    {/* <Checkbox checked={value === "true"} /> */}
-                    <Switch />
-                  </div>
-                );
-              } else if (objectType.properties[key].type === "number") {
-                return (
-                  <div key={key}>
-                    <Label>{objectType.properties[key].name}</Label>
-                    <div className="relative">
-                      <Input
-                        value={property.valueNumber}
-                        type="number"
-                        className="pl-10"
-                        onChange={(e) => {
-                          const draft = { ...object };
-                          const newObject = produce(draft, (draft) => {
-                            if (!draft.properties) {
-                              draft.properties = {};
-                            }
-                            draft.properties[key].valueNumber = Number(
-                              e.target.value
-                            );
-                          });
-                          mutate(newObject);
-                        }}
-                      />
-                      <HashIcon
-                        className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400 "
-                        size={12}
-                      />
-                    </div>
-                  </div>
-                );
-              } else if (objectType.properties[key].type === "date") {
-                return (
-                  <div key={key}>
-                    <Label>{objectType.properties[key].name}</Label>
-                    <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <Button
-                          variant={"outline"}
-                          className="w-full justify-start text-left font-normal text-muted-foreground"
-                        >
-                          <CalendarIcon className="mr-2" size={12} />
-                          {property?.valueDate
-                            ? new Date(property.valueDate).toLocaleDateString()
-                            : "Pick a date"}
-                        </Button>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent className="w-auto p-0">
-                        <DropdownMenuItem className="focus:bg-primary-background hover:bg-primary-background">
-                          <Calendar
-                            mode="single"
-                            initialFocus
-                            onDayClick={(day) => {
-                              const draft = { ...object };
-                              const newObject = produce(draft, (draft) => {
-                                if (!draft.properties) {
-                                  draft.properties = {};
-                                }
-                                draft.properties[key].valueDate =
-                                  day.toISOString();
-                              });
-                              mutate(newObject);
-                            }}
-                          />
-                        </DropdownMenuItem>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                  </div>
-                );
-              }
+                </div>
+              );
+            } else if (objectType.properties[key].type === "date") {
               return (
                 <div key={key}>
                   <Label>{objectType.properties[key].name}</Label>
@@ -170,13 +137,7 @@ const PropertiesSidebar = memo(
                 </div>
               );
             }
-            //   (
-            //     <p>
-            //       {objectType && objectType.properties[key].name}: {value} -{" "}
-            //       {objectType && objectType.properties[key].type}
-            //     </p>
-            //   )
-          )}
+          })}
         {/* <Button>Add Property</Button> */}
         {!object.properties && <>No properties found</>}
       </div>
